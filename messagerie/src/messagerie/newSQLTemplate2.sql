f1c4d54153924b13aa063c1c76015cebd24ad1a8
ayaberhil <147451152+ayaberhil@users.noreply.github.com>
2023-10-16 23:59:01 +0100

    Delete messagerie directory

--- messagerie/src/messagerie/newSQLTemplate2.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE message
-ADD CONSTRAINT FK_Message_Employe_E
-FOREIGN KEY (idE) REFERENCES employe(id);
-ALTER TABLE message
-ADD CONSTRAINT FK_Message_Employe_R
-FOREIGN KEY (idR) REFERENCES employe(id);
